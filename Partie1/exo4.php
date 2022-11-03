495256396c68137f1f62e75d60ad1e60e48449dd
derdeche <foued.derdeche@gmail.com>
2022-11-03 11:33:59 +0100

    exo4

--- Partie1/exo4.php
+++ Partie1/exo4.php
@@ -1,5 +1,17 @@
-<h1>exercice 4</h1>
+<h1>exercice 4</h1> 
 <p>Ecrire un algorithme permettant de savoir si une phrase est palindrome.</p>
+
 <h2>Résultat</h2>
 
 <?php
+$phrase = "Engage le jeu que je le gagne";
+$minus = strtoupper("$phrase"); /* mettre tt les majuscules en minuscules*/
+$replaced = str_replace(' ', '',$minus); /*enlever les espaces*/
+strrev("$replaced"); /*inverser la phrase*/
+if ($replaced == strrev("$replaced")){
+    echo "$phrase est une phrase palindrome";
+}
+else {
+    echo "$phrase n'est pas une phrase palindrome";
+}
+

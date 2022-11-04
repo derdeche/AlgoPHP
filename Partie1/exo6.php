b574438a7c12b86caa9aeafa86682a663a9c1e46
derdeche <you@example.com>
2022-11-04 16:10:19 +0100

    modif

---
+++ Partie1/exo6.php
@@ -0,0 +1,12 @@
+<h1>Exercice6</h1>
+<p>Ecrire un algorithme permettant de calculer un montant de facture à régler à partir de la quantité d’articles, 
+son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)</p>
+<h2>Résultat</h2>
+
+<?php
+$unit = '9.99';
+$ht= $unit*5;
+$mtva = $ht*0.2;
+$facture = $mtva+$ht;
+echo"Le montant de la facture à régler est de $facture";
+

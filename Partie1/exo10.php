b92a552d5515057c76d33fa4d98e05cf434164d7
derdeche <foued.derdeche@gmail.com>
2022-11-09 15:43:25 +0100

    modif

--- Partie1/exo10.php
+++ Partie1/exo10.php
@@ -1,2 +1,10 @@
 <h1>Exercice10</h1>
-<p>
+<p>A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui affiche à un utilisateur 
+    un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et 1 €.</p>
+<h2>Résultat</h2>
+
+<?php
+
+$MPayer = "150";
+$MVerse = "200";
+ echo "Montant à payer : $MPayer <br> ";

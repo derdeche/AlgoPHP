e64eee793efddb8374d3e532f6d1c7fac929df51
derdeche <foued.derdeche@gmail.com>
2022-11-09 11:59:34 +0100

    modif

--- Partie1/exo8.php
+++ Partie1/exo8.php
@@ -3,7 +3,33 @@
     d’un nombre
  passé en paramètre sous la forme:</p>
 
- <h2>Résultat</h2>
+ <h2>Résultat1</h2>
+ 
  <?php
+
+
+ $nombre = '2';    
+ 
+ 
+for ($i='1'; $i<='10'; $i++)
+{
+    $résultat= $nombre * $i;
+    echo $nombre . " * " .  $i . " = " . $résultat . "<br>";
+} 
+
+echo "2 éme methode" . "<br>";
+
+$nombre = '6';
+$i = '1';
+while ( $i<='10')
+{
+    $résultat = $nombre * $i;
+    echo $nombre . " * " .  $i  . " = " . $résultat . "<br>";
+    $i++;
+}
+
+
+
+
  
  

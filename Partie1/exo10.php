c983acb749d45baeb2bd392143d321c785892745
derdeche <foued.derdeche@gmail.com>
2022-11-09 16:14:14 +0100

    modif

--- Partie1/exo10.php
+++ Partie1/exo10.php
@@ -5,6 +5,21 @@
 
 <?php
 
-$MPayer = "150";
-$MVerse = "200";
+$MPayer = "152";
+$MVersé = "200";
+$reste = ($MVersé - $MPayer);
  echo "Montant à payer : $MPayer <br> ";
+ echo " Montant versé : $MVersé <br>";
+ echo " Reste à payer : $reste <br>";
+ $billet10 = intdiv($reste, 10);
+ $reste =  $reste - 10* $billet10;
+ $billet5 = intdiv($reste,5);
+ $reste = $reste - 5* $billet5;
+ $piece2 = intdiv($reste,2) ;
+ $reste = $reste - 2* $piece2;
+ $piece1 = intdiv($reste,1);
+ echo "$billet10 billets de 10 € - $billet5 billet de 5 € - $piece2 pièce de 2 € - $piece1 pièce de 1 €";
+
+ 
+
+

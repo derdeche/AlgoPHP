23e995620249256322710dcaf642656acfedb87b
derdeche <foued.derdeche@gmail.com>
2022-11-09 16:37:36 +0100

    modif

---
+++ Partie1/exo11.php
@@ -0,0 +1,8 @@
+<h1>Exercice11</h1>
+<p>Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce tableau et 
+    d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé d’afficher le nombre de marques 
+    de voitures présentes dans le tableau.</p>
+<h2>Résultat</h2>
+
+<?php
+

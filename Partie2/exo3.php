05fbb6352fa168f76faf0516da8e9c2824c68598
derdeche <antonin.pariss@gmail.com>
2022-11-14 16:56:13 +0100

    modif

--- Partie2/exo3.php
+++ Partie2/exo3.php
@@ -1,13 +1,13 @@
 <h1>Exercice3</h1>
 <p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir dans un nouvel onglet (_blank).</p>
 <h2>Résultat</h2>
-<p><a href="https://elan-formation.eu/accueil" target="_blank">ELAN!</a></p>
+
 
 
 <?php
 
 $link= "https://elan-formation.eu/accueil";
 
-echo "<p>appuyez sur ce lien <a href='https://elan-formation.eu/accueil' target='_blank'>ELAN!</a>' pour acceder au site de d'ELAN </p>";
+echo "appuyez sur ce lien <a href='$link' target='_blank'>ELAN!</a> pour acceder au site de d'ELAN";
 
 ?>

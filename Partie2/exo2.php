5f61f818a48e121b2a51592872bcec837b714c8e
derdeche <antonin.pariss@gmail.com>
2022-11-14 16:05:26 +0100

    modif

--- Partie2/exo2.php
+++ Partie2/exo2.php
@@ -8,9 +8,110 @@ grâce à une fonction personnalisée.</p><br>
 <p>Affichage<br>Pays Capitale<br>ALLEMAGNE Berlin<br>FRANCE Paris<br>ITALIE Rome<br>USA Washington</p>
 
 <h2>Résultat</h2>
+<!----
+<table border=1>
+    
+    <thead>
+        <tr>
+            <th>Pays</th>
+            <th>Capitale</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+        <tr>
+            <td>France</td>
+            <td>Paris</td>
+
+        </tr>
+        <tr>
+            <td>Allemagne</td>
+            <td>Berlin</td>
+
+        </tr>
+        <tr>
+            <td>Italie</td>
+            <td>rome</td>
+
+        </tr>
+        <tr>
+            <td>USA</td>
+            <td>Washington</td>
+
+        </tr>
+    </tbody>
+</table>
+----> 
+
+
+
+
+
 
 <?php
 
 
 
+$capitales = [
+
+    "France"=>"Paris",
+
+    "Allemagne"=>"Berlin",
+
+    "USA"=>"Washington",
+
+    "Italie"=>"Rome"
+
+];
+
+
+
+ foreach($capitales as $pays => $capitale){
+
+    echo "le pays $pays a pour capitale : $capitale<br>";
+
+ }
+
+
+echo afficherTableHtml($capitales);
+
+function afficherTableHtml($capitales) {
+
+    $result = "<table border=1>
+
+                <thead>
+
+                    <tr>
+
+                     <th>Pays</th>
+
+                     <th>Capitales</th>
+
+                    </tr>
+
+                </thead>
+
+          <tbody>" ;
+
+foreach ($capitales as $pays => $capitale){
+
+    $result .= "<tr>
+
+                    <td>$pays</td>
+
+                    <td>$capitale</td>
+
+                </tr>";
+
+
+}
+
+
+$result .= "</tbody></table>";
+
+return $result;
+
+}
+
+?>
 

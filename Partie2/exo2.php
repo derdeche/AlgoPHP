2339ee0b48e0b3645c76d87d86c4893f0ffce286
derdeche <antonin.pariss@gmail.com>
2022-11-14 11:59:29 +0100

    exo 1 + 2

---
+++ Partie2/exo2.php
@@ -0,0 +1,16 @@
+<h1>Exercice2</h1> 
+
+<p>Soit le tableau suivant:</p><br>
+<p>$capitales= array ("France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome");</p><br>
+<p>Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays)
+grâce à une fonction personnalisée.</p><br>
+<p>Vous devrez appeler la fonction comme suit: afficherTableHTML($capitales);</p><br>
+<p>Affichage<br>Pays Capitale<br>ALLEMAGNE Berlin<br>FRANCE Paris<br>ITALIE Rome<br>USA Washington</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+
+
+

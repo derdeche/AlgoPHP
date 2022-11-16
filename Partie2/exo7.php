9d61bb608acc544e16ddff35602327a7bc59cf9a
derdeche <foued.derdeche@gmail.com>
2022-11-16 16:48:39 +0100

    modif

---
+++ Partie2/exo7.php
@@ -0,0 +1,12 @@
+<h1>Exercice7</h1>
+<p>Créer une fonction personnalisée permettant de générer des cases à cocher. On pourra préciser dans le 
+tableau associatif si la case est cochée ou non.</P>
+<h2>Exemple :</h2>
+<p>genererCheckbox($elements);<br>
+//où $elements est un tableau associatif clé => valeur avec 3 entrées.</P>
+
+<h3>Résultat</h3>
+
+<?php   
+
+$element

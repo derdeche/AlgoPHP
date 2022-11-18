c93e62d15dde485f3f13ee5989d8977f83ffd132
derdeche <foued.derdeche@gmail.com>
2022-11-18 15:32:50 +0100

    modif

---
+++ Partie2/exo11.php
@@ -0,0 +1,8 @@
+<h1>Exercice11</h1>
+
+<p>En utilisant l’ensemble des fonctions personnalisées crées précédemment, créer un formulaire complet qui contient 
+les informations suivantes : champs de texte avec nom, prénom, adresse e-mail, ville, sexe et une liste de choix parmi lesquels 
+on pourra sélectionner un intitulé de formation : « Développeur Logiciel », « Designer web », « Intégrateur » ou « Chef de projet ».</P>
+<p>Le formulaire devra également comporter un bouton permettant de le soumettre à un traitement de validation (submit).</P>
+
+<h3>Résultat</h3>
